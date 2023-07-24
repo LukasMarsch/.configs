aaa8eae3d859ad1d43ec93762161415b4338e8b9
LukasMarsch <lmarsch@vater-gruppe.de>
2023-07-24 10:41:35 +0200

    init

---
+++ README.md
@@ -0,0 +1,51 @@
+# infinity-nvim
+## an infinity-colored nvim config for Windows
+---
+
+All commands starting with `>` are to be executed in powershell
+
+If you don't feel comfortable with the command line yet, start with a barebones nvim install and do the `:Tutor`
+to install:
+
+- Install [Alacritty](https://alacritty.org)
+- Install latest pwsh version
+
+    \>`winget install pwsh`
+
+- (install my [infinity-alacritty](https://github.com/LukasMarsch/infinity-alacritty) theme for a matching terminal experience)   
+- or configure alacritty however you please
+- Install [JetBrains Mono Nerd Font](https://github.com/ryanoasis/nerd-fonts/releases/latest) and import it to Windows
+- Install Neovim
+
+    \>`winget install --id=Neovim.Neovim  -e`
+
+- Copy the nvim folder from this project into $HOME\AppData\Local\ folder
+
+    \>`cp .\nvim ~\AppData\Local\ -Recurse`
+
+- Install [Plug Package Manager](https://github.com/junegunn/vim-plug#windows-powershell-1) for Neovim 
+
+- Start Neovim by opening Alacritty and typing
+
+    \>`nvim .`
+
+- If you are greeted with netrw, you have done everything correct!
+- Ensure you are in Normal mode (see bottom left), if not press `<Esc>`
+- Type `:PlugInstall` to install all configured extensions
+- This includes:
+  - nvim-telescope/telescope.nvim
+  - nvim-lua/plenary.nvim
+  - nvim-tree/nvim-web-devicons
+  - nvim-lualine/lualine.nvim
+  - nvim-treesitter/nvim-treesitter
+  - catppuccin/nvim
+  - nvim-treesitter/playground
+  - neovim/nvim-lspconfig
+  - williamboman/mason.nvim
+  - williamboman/mason-lspconfig.nvim
+  - hrsh7th/nvim-cmp
+  - hrsh7th/cmp-nvim-lsp
+  - L3MON4D3/LuaSnip
+  - VonHeikemen/lsp-zero.nvim
+- I encourage you to read all their documentation to understand my config
+- This config is not set in stone, feel free to mix it up to make it your personal editor <3
---
+++ nvim/init.lua
@@ -0,0 +1,157 @@
+require('remap')
+require('plug')
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.numberwidth = 4
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.termguicolors = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "160"
+
+require('nvim-web-devicons').setup {
+	r_icons = true;
+	default = true;
+	strict = true;
+
+	override_by_filename = {
+		[".gitignore"] = {
+		icon = "",
+		color = "#f1502f",
+		name = "Gitignore"
+		}
+	};
+}
+
+require('lualine').setup {
+	options = {
+		icons_enabled = true,
+		theme = 'auto',
+		component_separators = { left = '', right = ''},
+		section_separators = { left = '', right = ''},
+		disabled_filetypes = {
+		statusline = {},
+		winbar = {},
+	},
+	ignore_focus = {},
+	always_divide_middle = true,
+	globalstatus = false,
+	refresh = {
+		statusline = 1000,
+		tabline = 1000,
+		winbar = 1000,
+		}
+	},
+	sections = {
+		lualine_a = {'mode'},
+		lualine_b = {'branch', 'diff', 'diagnostics'},
+		lualine_c = {'filename'},
+		lualine_x = {'encoding', 'fileformat', 'filetype'},
+		lualine_y = {'progress'},
+		lualine_z = {'location'}
+		},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = {'filename'},
+		lualine_x = {'location'},
+		lualine_y = {},
+		lualine_z = {}
+	},
+	tabline = {},
+	winbar = {},
+	inactive_winbar = {},
+	extensions = {}
+}
+require'nvim-treesitter.configs'.setup {
+-- A list of parser names, or "all" (the five listed parsers should always be installed)
+ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust"},
+-- Install parsers synchronously (only applied to `ensure_installed`)
+sync_install = false,
+-- Automatically install missing parsers when entering buffer
+-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+auto_install = true,
+-- List of parsers to ignore installing (for "all")
+ignore_install = { "javascript" },
+---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+highlight = {
+	enable = true,
+	disable = {},
+	-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+	disable = function(lang, buf)
+	local max_filesize = 100 * 1024 -- 100 KB
+	local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+	if ok and stats and stats.size > max_filesize then
+	return true
+	end
+	end,
+	-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+	-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+	-- Using this option may slow down your editor, and you may see some duplicate highlights.
+	-- Instead of true it can also be a list of languages
+	},
+}
+
+
+local lsp = require('lsp-zero').preset({})
+lsp.on_attach(function(client, bufnr)
+	lsp.default_keymaps({buffer = bufnr})
+end)
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+lsp.setup()
+
+require('catppuccin').setup {
+	flavour = 'mocha',
+	background = {
+		light = 'latte',
+		dark = 'mocha',
+	},
+	dim_inactive = {
+		enabled = true,
+		shade = 'dark',
+		percentage = 0.15,
+	},
+	styles = {
+		comments = { 'italic' },
+		contitionals = { 'bold' },
+		loops = {},
+		functions = {},
+		keywords = {},
+		strings = {},
+		variables = {},
+		numbers = {},
+		booleans = {},
+		properties = {},
+		types = {},
+		operators = {},
+	},
+	color_overrides = {
+		mocha = {
+			overlay0 = '#ffffff',
+			surface2 = '#d4a935',
+			base = '#1E222A',
+			mantle = '#1c212b',
+		},
+	},
+}
+
+vim.cmd("colorscheme catppuccin")
+
+
